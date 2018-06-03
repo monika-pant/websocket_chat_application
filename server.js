bb7e34d0c4feb2e73d76ef6353f63d7bfa6a8f72
monika-pant <monikapant.1feb@gmail.com>
2018-06-03 13:15:11 +0530

    user presence addition

--- server.js
+++ server.js
@@ -1,31 +1,82 @@
 var socket_server= require('ws').Server;
 var s= new socket_server({port:8080});
+var presenceCounter=0;
+var clientNames=[];
 
 
 s.on('connection',function(ws){
+    presenceCounter++;
+    s.clients.forEach(function(client){  
+        var conOpendata;            
+        ws.on('message',function(data){
+                       
+            conOpendata=JSON.parse(data);           
+            if(client!= ws){
+                console.log('data type from client',conOpendata) 
+                if(conOpendata.type=='name'){
+                    client.send(JSON.stringify({
+                        users: presenceCounter,
+                        presence: "online"  ,
+                        type:'notify',
+                        // name:conOpendata.name  ,
+                        list:clientNames
+                    })); 
+                }
+                                         
+            } 
+            else{
+                if(conOpendata.type=='name'){
+                    client.send(JSON.stringify({
+                        users: presenceCounter,
+                        presence: "online"  ,
+                        type:'open',
+                        name:conOpendata.name  ,
+                        list:clientNames
+                    }));
+                }
+                  
+            } 
+            console.log('what from client',conOpendata)  
+                            
+        });                           
+    });    
+
+    //cient server interaction via message
     ws.on('message',function(msg){
         msg=JSON.parse(msg);
-        console.log(msg);
+        console.log('msg from client',msg)
+              
         //if the request is made to send name of the client then execute this
         if(msg.type == 'name'){
             //associating a new property with ws , so that every client can have a name identifier
-            ws['personName']=msg.name;
-            // console.log(ws);
+            ws['personName']=msg.name;            
             return;
         }
-        console.log('received: '+msg);
-        // ws.send('<span class="msg-label">Server: </span>'+msg);
         s.clients.forEach(function e(client){
             if(client!=ws){
                 client.send(JSON.stringify({
                     name: ws.personName,
-                    data: msg.data                    
+                    data: msg.data  ,
+                    totalUsers:presenceCounter  ,
+                    type:'message',
+                    presence:'online'              
                 }));
             }
-        });
+           
+        });        
     });
-    ws.on('close',function(msg){
-        console.log('oops!!! client lost');
+    ws.on('close',function(con){
+        presenceCounter--;
+        s.clients.forEach(function(client){
+                client.send(JSON.stringify({
+                    name: ws.personName,
+                    presence: "offline" ,
+                    type:'close',
+                    users:presenceCounter                   
+                }));
+                return;           
+        });                 
     });
-    console.log('client connected');
+    // console.log('client connected');   
+    
 });
